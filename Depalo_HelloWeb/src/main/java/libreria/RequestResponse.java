a11b1ca0679df74976824ff95bbce93b84b97cd9
Angelo071206 <depalo.angelo06@gmail.com>
2025-03-19 09:27:13 +0100

    Add files via upload

---
+++ Depalo_HelloWeb/src/main/java/libreria/RequestResponse.java
@@ -0,0 +1,22 @@
+package libreria;
+
+import java.io.IOException;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.core.Response;
+
+public class RequestResponse<E> {
+	
+	Response toResponse(E exception) {
+		return null;
+		
+	}
+
+	public void filter(ContainerRequestContext requestContext)
+			throws IOException {
+	}
+	
+	public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext)
+			throws IOException {
+	}
+}
