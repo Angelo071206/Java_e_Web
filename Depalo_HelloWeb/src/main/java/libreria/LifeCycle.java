9c5c64274faf9e75f8b77850e5e41dc8dd3b5f9c
Angelo071206 <depalo.angelo06@gmail.com>
2025-03-23 18:59:12 +0100

    Add files via upload

---
+++ Depalo_HelloWeb/src/main/java/libreria/LifeCycle.java
@@ -0,0 +1,14 @@
+package libreria;
+
+import java.awt.Container;
+
+public class LifeCycle {
+	public void onStartup(Container container) {
+	}
+	
+	public void onReload(Container container) {
+	}
+	
+	public void onShutdown(Container container) {
+	}
+}
